c170a35cd3306df0c0369330c6cb8f16b751d58e
Brilliant-1f48e <brilliant1f48e@gmail.com>
2023-02-07 20:29:27 +0700

    add plugin tabs

---
+++ src/components/tabs/tabs.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Tabs{
+    constructor(selector, options){
+        this.el = document.querySelector(selector);
+        this.options = options;
+
+        this._setup;
+    }
+
+    _setup(){
+    }
+}
