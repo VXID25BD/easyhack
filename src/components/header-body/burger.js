c65048852880e6255e65251dfa6e3f328be5653d
Brilliant-1f48e <brilliant1f48e@gmail.com>
2023-02-10 22:13:27 +0700

    refactoring header-body adaptive, add menu burger script

---
+++ src/components/header-body/burger.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const menuButton = document.querySelector(".menu__button");
+
+if (menuButton){
+    menuButton.addEventListener("click", e => {
+        document.querySelector(".menu").classList.toggle("_open");
+        e.target.classList.toggle("_open");
+        document.body.classList.toggle("_lock");
+    });
+}
